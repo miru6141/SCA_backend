9fa2ba7780e5f9f6af297622981ad4a8655e08c3
miru6141 <miru6141@gmail.com>
2024-07-03 19:20:51 +0530

    change

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ dotenv.config({
     path:"../.env"
 })
 const PORT =process.env.PORT || 3000
-const DB_URL=process.env.DB_URL
+const DB_URL=process.env.DB_URL || "mongodb://127.0.0.1:27017/E-learning";
 
 const app=express();
 app.use(express.json());
